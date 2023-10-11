70fb24e31f0dcd73445a8c4b5499fe29592c183c
Ivan Grgić <ivan.grgic5@gmail.com>
2023-10-11 16:16:22 +0200

    feature: Single and multiple selecting now available. Reduced some css.

--- index.js
+++ index.js
@@ -3,41 +3,62 @@ const selectOptions = [
   { value: "2", label: "Cikla" },
   { value: "3", label: "Grah" },
   { value: "4", label: "Paprika" },
-
   // ... more options
 ];
 
+const styling = {
+  li: "margin: 5px; list-style: none; border-radius: 5px; padding-left: 10px; text-align: center;",
+  ul: "display: flex; flex-flow: column nowrap; margin: 0px; padding-left: 0;",
+};
+
 const settings = {
-  theme: "light", // light, dark
-  search: true, // will enable search input in dropdown and search values
-  multiSelect: false, // will enable multi selection of values
+  theme: "dark", // light, dark
+  search: false, // will enable search input in dropdown and search values
+  multiSelect: true, // will enable multi selection of values
   dropdownButtonLabel: "Vegetables",
 };
 //const {theme, search, dropdownButtonLabel, multiSelect} = settings;
 
 const selectValues = [];
+let selectValue = null;
 
 function createOptions(options, location) {
   options.forEach((option) => {
     let dropdownOption = document.createElement("li");
+    dropdownOption.style.cssText = styling.li;
     dropdownOption.setAttribute("value", option.value);
     dropdownOption.innerText = option.label;
     dropdownOption.classList.add(settings.theme);
-    dropdownOption.addEventListener("click", () => {
-      if (!dropdownOption.classList.contains("selected")) {
-        dropdownOption.classList.add("selected");
-        let selectedOptions = document.getElementsByClassName("selected");
-        let values = Object.values(selectedOptions);
-        selectValues.splice(0);
-        values.forEach((item) => selectValues.push(item.value));
-        console.log(selectedOptions, values, selectValues);
+    dropdownOption.addEventListener("click", (event) => {
+      if (settings.multiSelect) {
+        if (!dropdownOption.classList.contains("selected")) {
+          dropdownOption.classList.add("selected");
+          let selectedOptions = document.getElementsByClassName("selected");
+          let values = Object.values(selectedOptions);
+          selectValues.splice(0);
+          values.forEach((item) => selectValues.push(item.value));
+          console.log(selectedOptions, values, selectValues);
+        } else {
+          dropdownOption.classList.remove("selected");
+          let selectedOptions = document.getElementsByClassName("selected");
+          let values = Object.values(selectedOptions);
+          selectValues.splice(0);
+          values.forEach((item) => selectValues.push(item.value));
+          console.log(selectedOptions, "<EEE", values, selectValues);
+        }
       } else {
-        dropdownOption.classList.remove("selected");
         let selectedOptions = document.getElementsByClassName("selected");
         let values = Object.values(selectedOptions);
-        selectValues.splice(0);
-        values.forEach((item) => selectValues.push(item.value));
-        console.log(selectedOptions, values, selectValues);
+        values.forEach((item) => item.classList.remove("selected"));
+        if (!event.target.classList.contains("selected")) {
+          event.target.classList.add("selected");
+          selectValue = event.target.value;
+          console.log(selectValue);
+        } else {
+          event.target.classList.remove("selected");
+          selectValue = null;
+          console.log(selectValue);
+        }
       }
     });
     location.appendChild(dropdownOption);
@@ -50,6 +71,7 @@ function create(selector, options, settings) {
   const createAt = document.getElementById(selector);
   createAt.style.width = "200px";
   const dropdown = document.createElement("ul");
+  dropdown.style.cssText = styling.ul;
   dropdown.style.display = "none";
 
   const searchInput = document.createElement("input");
